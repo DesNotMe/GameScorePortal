25bd1764c6cab4df25786823005bd11d6235099d
222528x <222528x@CL22G603>
2024-07-04 09:49:49 +0800

    changed about.cshtml.cs

--- GameScorePortal/Pages/About.cshtml.cs
+++ GameScorePortal/Pages/About.cshtml.cs
@@ -1,12 +1,31 @@
+using GameScorePortal.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GameScorePortal.Pages
 {
     public class AboutModel : PageModel
+{
+    private readonly GameScorePortalDbContext _context;
+
+    public AboutModel(GameScorePortalDbContext context)
     {
-        public void OnGet()
-        {
-        }
+        _context = context;
+    }
+
+    public IEnumerable<JoinDateCount> PlayerJoinStats { get; set; }
+
+    public void OnGet()
+    {
+        var players = _context.Players.ToList();
+        PlayerJoinStats = players.GroupBy(player => player.JoinDate.Date)
+                                 .Select(group => new JoinDateCount { JoinDate = group.Key, Count = group.Count() });
+    }
+}
+
+    public class JoinDateCount // This could be a separate model class
+    {
+        public DateTime JoinDate { get; set; }
+        public int Count { get; set; }
     }
 }
